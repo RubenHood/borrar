b903a69198a187a007db82b3705a5c67d11d548f
Ruben__hood <rubenconb88@gmail.com>
2019-07-02 17:51:13 +0200

    Setup first Api Method on Backend for testing

--- src/BackendBundle/Controller/Api.php
+++ src/BackendBundle/Controller/Api.php
@@ -4,14 +4,43 @@ namespace BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use BackendBundle\Entity\Product;
+
 
 class Api extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
+    //para convertir el json en informacion leíble
+    public function serializeProduct(Product $product)
     {
-        return $this->render('@Backend/Default/index.html.twig');
+        return array(
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+            'price' => $product->getPrice(),
+            'img' => $product->getImg(),
+            'category' => $product->getCategory()
+        );
+    }
+
+    public function result($clave, $valor = null)
+    {
+        return array($clave => $valor);
+    }
+
+    /**
+     * @Route("/", methods={"GET"})
+     */
+    public function getAllProducts()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository(Product::class)->findAll();
+
+        $data = array('products' => array());
+
+        foreach ($products as $product) {
+            $data['products'][] = $this->serializeProduct($product);
+        }
+        return new JsonResponse($data, 200);
     }
 }
